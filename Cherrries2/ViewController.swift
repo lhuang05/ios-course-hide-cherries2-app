b2c5fdf415ded929a64d88a107becc6d60cb9017
Lisa Huang <lisa.huang@verizon.com>
2016-04-07 11:21:52 -0700

    add all cherries2 files

--- Cherrries2/ViewController.swift
+++ Cherrries2/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueCherry: UIImageView!
+    @IBOutlet weak var redCherry: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideBlue(sender: AnyObject) {
+        blueCherry.hidden = true
+    }
 
+    @IBAction func hideRed(sender: AnyObject) {
+        redCherry.hidden = true
+    }
 }
 
